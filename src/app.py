635ce5b83b4b9cb221ee7a28b05ed73685fca2dd
Eduardo de la Garza <delagarzaguerra@gmail.com>
2017-10-04 11:49:37 -0500

    Heroku8

--- src/app.py
+++ src/app.py
@@ -16,7 +16,10 @@ app = Flask(__name__)
 app.config.from_object('src.config')
 app.secret_key = os.environ.get("SECRET_KEY")
 
-Database.initialize(minconn=1, maxconn=10, user=os.environ.get("USER"), password=os.environ.get("PASSWORD"), host=os.environ.get("HOST"), database=os.environ.get("DATABASE"))
+
+@app.before_first_request
+def init_db():
+	Database.initialize(minconn=1, maxconn=10, user=os.environ.get("USER"), password=os.environ.get("PASSWORD"), host=os.environ.get("HOST"), database=os.environ.get("DATABASE"))
 
 @app.route('/',methods=['GET', 'POST'])
 def homepage():
